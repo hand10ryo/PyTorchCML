a82f17a0e860349c8f431c8c393fdccd6e0cd4b5
hand10ryo <hand10ryo@yahoo.co.jp>
2021-04-18 17:21:57 +0000

    implement bias to mf model

--- src/PytorchCML/models/MatrixFactorization.py
+++ src/PytorchCML/models/MatrixFactorization.py
@@ -7,6 +7,42 @@ from .BaseEmbeddingModel import BaseEmbeddingModel
 
 
 class LogitMatrixFactorization(BaseEmbeddingModel):
+    def __init__(self, n_user: int, n_item: int, n_dim: int = 20,
+                 max_norm: Optional[float] = None,
+                 max_bias: Optional[float] = 1,
+                 user_embedding_init: Optional[torch.Tensor] = None,
+                 item_embedding_init: Optional[torch.Tensor] = None,
+                 user_bias_init: Optional[torch.Tensor] = None,
+                 item_bias_init: Optional[torch.Tensor] = None):
+        """
+        Args :
+            user_bias_init : 1d torch.Tensor size = (n_user)
+            item_bias_init : 1d torch.Tensor size = (n_item)
+        """
+
+        super().__init__(
+            n_user, n_item, n_dim, max_norm,
+            user_embedding_init, item_embedding_init
+        )
+        self.max_bias = max_bias
+
+        if user_bias_init is None:
+            self.user_bias = nn.Embedding(n_user, 1, max_norm=max_bias)
+
+        else:
+            self.user_bias = nn.Embedding.from_pretrained(
+                user_bias_init.reshape(-1, 1)
+            )
+            self.user_bias.weight.requires_grad = True
+
+        if item_bias_init is None:
+            self.item_bias = nn.Embedding(n_item, 1, max_norm=max_bias)
+
+        else:
+            self.item_bias = nn.Embedding.from_pretrained(
+                item_bias_init.reshape(-1, 1)
+            )
+            self.item_bias.weight.requires_grad = True
 
     def forward(self, users: torch.Tensor, items: torch.Tensor) -> torch.Tensor:
         """
@@ -28,10 +64,14 @@ class LogitMatrixFactorization(BaseEmbeddingModel):
         u_emb = self.user_embedding(users)  # batch_size × dim
         i_emb = self.item_embedding(items)  # batch_size × n_samples × dim
 
+        # get bias
+        u_bias = self.user_bias(users)  # batch_size × 1
+        i_bias = self.user_bias(users)  # batch_size × n_samples
+
         # compute inner product
         inner = torch.einsum('nd,njd->nj', u_emb, i_emb)
 
-        return inner
+        return inner + u_bias + i_bias
 
     def predict(self, pairs: torch.Tensor) -> torch.Tensor:
         """
