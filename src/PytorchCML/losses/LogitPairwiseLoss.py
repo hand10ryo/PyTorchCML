c1b437e03f9eea8398a40de23bada4a6dad710c8
hand10ryo <hand10ryo@yahoo.co.jp>
2021-04-27 00:40:40 +0000

    fix logitLoss

--- src/PytorchCML/losses/LogitPairwiseLoss.py
+++ src/PytorchCML/losses/LogitPairwiseLoss.py
@@ -30,12 +30,12 @@ class LogitPairwiseLoss(BasePairwiseLoss):
             neg_item_bias : bias of negative item size (n_batch, n_neg_samples, 1)
         """
         embeddings_dict = {
-            "user_emb": user_emb,
-            "pos_item_emb": pos_item_emb,
-            "neg_item_emb": neg_item_emb,
-            "user_bias": user_bias,
-            "pos_item_bias": pos_item_bias,
-            "neg_item_bias": neg_item_bias
+            "user_emb": user_emb,  # (B, d)
+            "pos_item_emb": pos_item_emb,  # (B, d)
+            "neg_item_emb": neg_item_emb,  # (B, N, d)
+            "user_bias": user_bias,  # (B, 1)
+            "pos_item_bias": pos_item_bias,  # (B, 1)
+            "neg_item_bias": neg_item_bias  # (B, N, 1)
         }
 
         n_batch = user_emb.shape[0]
@@ -46,7 +46,8 @@ class LogitPairwiseLoss(BasePairwiseLoss):
         pos_inner = torch.einsum('nd,nd->n', user_emb, pos_item_emb)
         neg_inner = torch.einsum('nd,njd->nj', user_emb, neg_item_emb)
 
-        pos_y_hat = pos_inner + user_bias + pos_item_bias
+        pos_y_hat = pos_inner + (user_bias + pos_item_bias).reshape(-1)
+        # pos_y_hat = pos_inner + user_bias + pos_item_bias
         neg_y_hat = neg_inner + user_bias + neg_item_bias
 
         pos_loss = - nn.LogSigmoid()(pos_y_hat).sum()
