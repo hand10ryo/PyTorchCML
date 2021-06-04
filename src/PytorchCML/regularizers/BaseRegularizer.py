c3ad43df6266d96ed6d66a30d48e023e49c5a4eb
hand10ryo <hand10ryo@yahoo.co.jp>
2021-06-04 12:42:42 +0000

    implement L2 Regularizer

--- src/PytorchCML/regularizers/BaseRegularizer.py
+++ src/PytorchCML/regularizers/BaseRegularizer.py
@@ -3,11 +3,12 @@ from torch import nn
 
 
 class BaseRegularizer(nn.Module):
-    def __init__(self):
+    def __init__(self, weight=1e-2):
         super().__init__()
+        self.weight = weight
 
     def forward(self, embeddings_dict: dict) -> torch.Tensor:
-        """ method of forwarding
+        """method of forwarding
 
         Args:
             embeddings_dict (dict): dictionary of embbedings which will be used.
